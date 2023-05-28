8b62aeca2294075aa986ac8a4c16e7a2799d7ef8
yier <1174043641@qq.com>
2023-05-28 14:41:31 +0800

    format

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <h1>我是1111</h1>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+for(let i=0; i<10;i++)
+console.log(i);
+
+</script>
 <style scoped>
 .color {
   width: 100px;
